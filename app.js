b930e0ec252782f5e373a487fada46df5f9a6afd
franromlob <franromlob@gmail.com>
2025-05-18 09:57:32 +0200

    add app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const http = require('http');
+// 1. Importamos el módulo 'http' de Node.js (para crear el servidor)
+const { Console } = require("console");
+const http = require("http");
 
+// 2. Definimos el puerto donde correrá el servidor (ej: 3000)
 const PORT = 3000;
 
+// 3. Creamos el servidor con createServer
 const server = http.createServer((req, res) => {
+  // 4. Configuramos el tipo de contenido de la respuesta HTML
+  res.setHeader("Content-Type", "text/html");
 
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1> Hola mundo Francisco Romero </h1>');
-
+  // 5. Enviamos una respuesta al cliente (contenido HTML)
+  res.end("<h1>Hola, mundo!</h1>");
 });
 
+// 6. Ponemos el servidor a 'escuchar' en el puerto definido
 server.listen(PORT, () => {
-  console.log(`Server running at http:localhost:${PORT}`)
-})
-
+  // 7. Esto se ejecuta cuando el servidor inicia correctamente
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
